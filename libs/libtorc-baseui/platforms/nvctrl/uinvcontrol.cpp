494cd62ee69bed277174d45a0c8f229ce3dabf72
Mark Kendall <mark.kendall@gmail.com>
2012-12-03 11:49:15 +0000

    libtorc-baseui: Finish EDID grabbing for NV-CONTROL

--- libs/libtorc-baseui/platforms/nvctrl/uinvcontrol.cpp
+++ libs/libtorc-baseui/platforms/nvctrl/uinvcontrol.cpp
@@ -31,6 +31,54 @@ extern "C" {
 
 QMutex* gNVCtrlLock = new QMutex(QMutex::Recursive);
 
+static QString DisplayDeviceName(int Mask)
+{
+    switch (Mask)
+    {
+    case (1 <<  0): return QString("CRT-0"); break;
+    case (1 <<  1): return QString("CRT-1"); break;
+    case (1 <<  2): return QString("CRT-2"); break;
+    case (1 <<  3): return QString("CRT-3"); break;
+    case (1 <<  4): return QString("CRT-4"); break;
+    case (1 <<  5): return QString("CRT-5"); break;
+    case (1 <<  6): return QString("CRT-6"); break;
+    case (1 <<  7): return QString("CRT-7"); break;
+    case (1 <<  8): return QString("TV-0"); break;
+    case (1 <<  9): return QString("TV-1"); break;
+    case (1 << 10): return QString("TV-2"); break;
+    case (1 << 11): return QString("TV-3"); break;
+    case (1 << 12): return QString("TV-4"); break;
+    case (1 << 13): return QString("TV-5"); break;
+    case (1 << 14): return QString("TV-6"); break;
+    case (1 << 15): return QString("TV-7"); break;
+    case (1 << 16): return QString("DFP-0"); break;
+    case (1 << 17): return QString("DFP-1"); break;
+    case (1 << 18): return QString("DFP-2"); break;
+    case (1 << 19): return QString("DFP-3"); break;
+    case (1 << 20): return QString("DFP-4"); break;
+    case (1 << 21): return QString("DFP-5"); break;
+    case (1 << 22): return QString("DFP-6"); break;
+    case (1 << 23): return QString("DFP-7"); break;
+    default:        return QString("Unknown");
+    }
+}
+
+int ListDisplays(int Displays)
+{
+    int count = 0;
+
+    for (int i = 1; i < (1 << 24); i <<= 1)
+    {
+        if (Displays & i)
+        {
+            count++;
+            LOG(VB_GENERAL, LOG_INFO, QString("Connected display: %1").arg(DisplayDeviceName(i)));
+        }
+    }
+
+    return count;
+}
+
 bool UINVControl::NVControlAvailable(void)
 {
     QMutexLocker locker(gNVCtrlLock);
@@ -93,11 +141,39 @@ QByteArray UINVControl::GetNVEDID(Display *XDisplay, int Screen)
             .arg(Screen).arg(XDisplayName(NULL)));
         return result;
     }
+    int displays = 0;
+    Bool ok = XNVCTRLQueryAttribute(XDisplay, Screen, 0, NV_CTRL_CONNECTED_DISPLAYS, &displays);
+    if (ok != True)
+    {
+        LOG(VB_GENERAL, LOG_ERR, "Failed to retrieve display list");
+        return result;
+    }
+
+    int displaycount = ListDisplays(displays);
+    if (displaycount != 1)
+    {
+        LOG(VB_GENERAL, LOG_WARNING, "There is more than one physical display attached to this screen. Ignoring EDID");
+        return result;
+    }
+
+    int edid = NV_CTRL_EDID_AVAILABLE_FALSE;
+    ok = XNVCTRLQueryAttribute(XDisplay, Screen, displays, NV_CTRL_EDID_AVAILABLE, &edid);
+    if (ok != True)
+    {
+        LOG(VB_GENERAL, LOG_INFO, "Failed to check EDID_AVAILABLE attribute");
+        return result;
+    }
+
+    if (edid != NV_CTRL_EDID_AVAILABLE_TRUE)
+    {
+        LOG(VB_GENERAL, LOG_INFO, "EDID not available");
+        return result;
+    }
 
     unsigned char* data = NULL;
     int datalength = 0;
 
-    Bool ok = XNVCTRLQueryBinaryData(XDisplay, Screen, 0, NV_CTRL_BINARY_DATA_EDID, &data, &datalength);
+    ok = XNVCTRLQueryBinaryData(XDisplay, Screen, 0, NV_CTRL_BINARY_DATA_EDID, &data, &datalength);
 
     if (ok != True)
     {
@@ -107,5 +183,6 @@ QByteArray UINVControl::GetNVEDID(Display *XDisplay, int Screen)
     }
 
     result = QByteArray((const char*)data, datalength);
+    delete data;
     return result;
 }
