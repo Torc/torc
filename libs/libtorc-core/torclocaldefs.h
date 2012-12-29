2c93e5407839e68326239316247a24687fd3ace3
Mark Kendall <mark.kendall@gmail.com>
2012-12-29 18:20:52 +0000

    libtorc-core: Suppress a windows warning

--- libs/libtorc-core/torclocaldefs.h
+++ libs/libtorc-core/torclocaldefs.h
@@ -1,7 +1,9 @@
 #ifndef TORCLOCALDEFS_H
 #define TORCLOCALDEFS_H
 
+#ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0600
+#endif
 
 #define TORC_MAIN_THREAD  QString("MainLoop")
 #define TORC_ADMIN_THREAD QString("AdminLoop")
