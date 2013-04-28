cbbf9669b75a50b88bec9486b432072704110fdd
Mark Kendall <mark.kendall@gmail.com>
2013-04-28 10:41:45 +0100

    libtorc-audio: Inline a simple function

--- libs/libtorc-audio/torcavutils.cpp
+++ libs/libtorc-audio/torcavutils.cpp
@@ -391,7 +391,7 @@ QString AVTimeToString(qint64 Time)
 }
 
 
-bool IsHardwarePixelFormat(AVPixelFormat Format)
+inline bool IsHardwarePixelFormat(AVPixelFormat Format)
 {
     return Format == AV_PIX_FMT_XVMC_MPEG2_MC   ||
            Format == AV_PIX_FMT_XVMC_MPEG2_IDCT ||
