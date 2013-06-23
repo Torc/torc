92d0ccadfe7198c0358b3802b40b8a34cb8db5c9
Mark Kendall <mark.kendall@gmail.com>
2013-06-23 21:21:21 +0100

    libtorc-core: Document TorcBonjour

--- libs/libtorc-core/torcbonjour.cpp
+++ libs/libtorc-core/torcbonjour.cpp
@@ -17,13 +17,6 @@
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-// Std
-#include <stdlib.h>
-#include <arpa/inet.h>
-
-// DNS Service Discovery
-#include <dns_sd.h>
-
 // Qt
 #include <QCoreApplication>
 #include <QSocketNotifier>
@@ -40,41 +33,58 @@
 #include "torchttpserver.h"
 #include "torcbonjour.h"
 
-TorcBonjour* gBonjour = NULL;
-QMutex*      gBonjourLock = new QMutex(QMutex::Recursive);
+// Std
+#include <stdlib.h>
+#include <arpa/inet.h>
 
-void DNSSD_API BonjourRegisterCallback(DNSServiceRef       Ref,
-                                       DNSServiceFlags     Flags,
-                                       DNSServiceErrorType Errorcode,
-                                       const char *Name, const char *Type,
-                                       const char *Domain, void *Object);
-void DNSSD_API BonjourBrowseCallback(DNSServiceRef        Ref,
-                                     DNSServiceFlags      Flags,
-                                     uint32_t             InterfaceIndex,
-                                     DNSServiceErrorType  ErrorCode,
-                                     const char          *Name,
-                                     const char          *Type,
-                                     const char          *Domain,
-                                     void                *Object);
-void DNSSD_API BonjourResolveCallback(DNSServiceRef        Ref,
-                                      DNSServiceFlags      Flags,
-                                      uint32_t             InterfaceIndex,
-                                      DNSServiceErrorType  ErrorCode,
-                                      const char          *Fullname,
-                                      const char          *HostTarget,
-                                      uint16_t             Port,
-                                      uint16_t             TxtLen,
-                                      const unsigned char *TxtRecord,
-                                      void                *Object);
+// DNS Service Discovery
+#include <dns_sd.h>
 
+TorcBonjour* gBonjour = NULL;                               //!< Global TorcBonjour singleton
+QMutex*      gBonjourLock = new QMutex(QMutex::Recursive);  //!< Lock around access to gBonjour
+
+void DNSSD_API BonjourRegisterCallback (DNSServiceRef        Ref,
+                                        DNSServiceFlags      Flags,
+                                        DNSServiceErrorType  Errorcode,
+                                        const char          *Name,
+                                        const char          *Type,
+                                        const char          *Domain,
+                                        void                *Object);
+void DNSSD_API BonjourBrowseCallback   (DNSServiceRef        Ref,
+                                        DNSServiceFlags      Flags,
+                                        uint32_t             InterfaceIndex,
+                                        DNSServiceErrorType  ErrorCode,
+                                        const char          *Name,
+                                        const char          *Type,
+                                        const char          *Domain,
+                                        void                *Object);
+void DNSSD_API BonjourResolveCallback  (DNSServiceRef        Ref,
+                                        DNSServiceFlags      Flags,
+                                        uint32_t             InterfaceIndex,
+                                        DNSServiceErrorType  ErrorCode,
+                                        const char          *Fullname,
+                                        const char          *HostTarget,
+                                        uint16_t             Port,
+                                        uint16_t             TxtLen,
+                                        const unsigned char *TxtRecord,
+                                        void                *Object);
+
+/*! \class TorcBonjourService
+ *  \brief Wrapper around a DNS service reference, either advertised or discovered.
+ *
+ * TorcBonjourService takes ownership of both the DNSServiceRef and QSocketNotifier object - to
+ * ensure resources are properly released, Deregister must be called.
+ *
+ * \sa TorcBonjour
+*/
 class TorcBonjourService
 {
   public:
     enum ServiceType
     {
-        Service,
-        Browse,
-        Resolve
+        Service, //!< A service being advertised by this application
+        Browse,  //!< An external service which we are actively trying to discover
+        Resolve  //!< Address resolution for a discovered service
     };
 
   public:
@@ -92,6 +102,8 @@ class TorcBonjourService
         m_fd(-1),
         m_socketNotifier(NULL)
     {
+        // the default constructor only exists to keep QMap happy - it should never be used
+        LOG(VB_GENERAL, LOG_WARNING, "Invalid TorcBonjourService object");
     }
 
     TorcBonjourService(ServiceType BonjourType, DNSServiceRef DNSSRef, const QByteArray &Name, const QByteArray &Type)
@@ -128,6 +140,9 @@ class TorcBonjourService
     {
     }
 
+    /*! \fn    TorcBonjourService::SetFileDescriptor
+     *  \brief Sets the file descriptor and creates a QSocketNotifier to listen for socket events
+    */
     void SetFileDescriptor(int FileDescriptor, QObject *Object)
     {
         if (FileDescriptor != -1 && Object)
@@ -140,11 +155,17 @@ class TorcBonjourService
         }
     }
 
+    /*! \fn    TorcBonjourService::IsResolved
+     *  \brief Returns true when the service has been fully resolved to an IP address and port.
+    */
     bool IsResolved(void)
     {
         return m_port && !m_ipAddresses.isEmpty();
     }
 
+    /*! \fn    TorcBonjourService::Deregister
+     *  \brief Release all resources associated with this service.
+     */
     void Deregister(void)
     {
         if (m_socketNotifier)
@@ -154,6 +175,7 @@ class TorcBonjourService
         {
             LOG(VB_NETWORK, LOG_WARNING, QString("Host lookup for '%1' is not finished - cancelling").arg(m_host.data()));
             QHostInfo::abortHostLookup(m_lookupID);
+            m_lookupID = -1;
         }
 
         if (m_dnssRef)
@@ -200,6 +222,14 @@ class TorcBonjourService
     QSocketNotifier *m_socketNotifier;
 };
 
+/*! \class TorcBonjourPriv
+ *  \brief Private implementation of Bonjour service registration and browsing.
+ *
+ * \sa TorcBonjour
+ * \sa TorcBonjourService
+ * \sa TorcNetworkedContext
+ * \sa TorcRAOPDevice
+*/
 class TorcBonjourPriv
 {
   public:
@@ -209,6 +239,7 @@ class TorcBonjourPriv
         m_serviceLock(new QMutex(QMutex::Recursive)),
         m_discoveredLock(new QMutex(QMutex::Recursive))
     {
+        // the Avahi compatability layer on *nix will spam us with warnings
         qputenv("AVAHI_COMPAT_NOWARN", "1");
     }
 
@@ -249,6 +280,14 @@ class TorcBonjourPriv
         return m_suspended;
     }
 
+/*! \fn    TorcBonjourPriv::Suspend
+ *  \brief Suspend all Bonjour activities.
+ *
+ * This method will deregister all existing service announcements but retain
+ * the details for each. On resumption, services are then re-registered.
+ *
+ * \sa Resume
+*/
     void Suspend(void)
     {
         if (m_suspended)
@@ -280,6 +319,11 @@ class TorcBonjourPriv
         }
     }
 
+    /*! \fn    TorcBonjourPriv::Resume
+     *  \brief Resume Bonjour activities.
+     *
+     * \sa Suspend
+    */
     void Resume(void)
     {
         if (!m_suspended)
@@ -309,9 +353,22 @@ class TorcBonjourPriv
         }
     }
 
+/*! \fn    TorcBonjourPriv::Register
+ *  \brief Register a service with Bonjour.
+ *
+ * Register the service described by Name, Type, Txt and Port. If Bonjour
+ * activities are currently suspended, the details are saved and processed
+ * when Bonjour is available - in which case a reference is still returned
+ * (i.e. it does not return an error).
+ *
+ * \param Reference Used to re-create a service registration following a Resume.
+ * \returns An opaque reference to be passed to Deregister when complete or 0 on error.
+ *
+ * \sa Deregister
+*/
     quint32 Register(quint16 Port, const QByteArray &Type,
-                   const QByteArray &Name, const QByteArray &Txt,
-                   quint32 Reference = 0)
+                     const QByteArray &Name, const QByteArray &Txt,
+                     quint32 Reference = 0)
     {
         if (m_suspended)
         {
@@ -321,6 +378,7 @@ class TorcBonjourPriv
             service.m_txt  = Txt;
             service.m_port = Port;
             quint32 reference = Reference;
+            // find a unique reference
             while (!reference || m_services.contains(reference))
                 reference++;
             m_services.insert(reference, service);
@@ -361,6 +419,14 @@ class TorcBonjourPriv
         return 0;
     }
 
+    /*! \fn    TorcBonjourPriv::Browse
+     *  \brief Search for a service named Type
+     *
+     * \param   Reference Used to re-create a Browse following a Resume.
+     * \returns An opaque reference to be passed to Deregister when complete or 0 on error.
+     *
+     * \sa Deregister
+    */
     quint32 Browse(const QByteArray &Type, quint32 Reference = 0)
     {
         static QByteArray dummy("browser");
@@ -405,6 +471,14 @@ class TorcBonjourPriv
         return 0;
     }
 
+    /*! \fn    TorcBonjourPriv::Deregister
+     *  \brief Cancel a Bonjour announcement or browse.
+     *
+     * \param Reference An opaque reference returned from Register or Browse.
+     *
+     * \sa Register
+     * \sa Browse
+    */
     void Deregister(quint32 Reference)
     {
         QByteArray type;
@@ -441,9 +515,17 @@ class TorcBonjourPriv
         }
     }
 
+/*! \fn    TorcBonjourPriv::SocketReadyRead
+ *  \brief Handle new socket data
+ *
+ * Socket notifications are passed from the TorcBonjour parent and passed to
+ * the Bonjour library for handling. Any further action is triggered via callbacks
+ * from Bonjour.
+*/
     void SocketReadyRead(int Socket)
     {
         {
+            // match Socket to an announced service
             QMutexLocker lock(m_serviceLock);
             QMap<quint32,TorcBonjourService>::iterator it = m_services.begin();
             for ( ; it != m_services.end(); ++it)
@@ -459,6 +541,7 @@ class TorcBonjourPriv
         }
 
         {
+            // match Socket to a discovered service
             QMutexLocker lock(m_discoveredLock);
             QMap<quint32,TorcBonjourService>::iterator it = m_discoveredServices.begin();
             for ( ; it != m_discoveredServices.end(); ++it)
@@ -476,6 +559,12 @@ class TorcBonjourPriv
         LOG(VB_GENERAL, LOG_WARNING, "Read request on unknown socket");
     }
 
+/*! \fn    TorcBonjourPriv::AddBrowseResult
+ *  \brief Handle newly discovered service.
+ *
+ * Services that have not been seen before are added to the list of known services
+ * and a name resolution is kicked off.
+*/
     void AddBrowseResult(DNSServiceRef Reference,
                          const TorcBonjourService &Service)
     {
@@ -545,6 +634,9 @@ class TorcBonjourPriv
         }
     }
 
+    /*! \fn    TorcBonjourPriv::RemoveBrowseResult
+     *  \brief Handle removed services.
+    */
     void RemoveBrowseResult(DNSServiceRef Reference,
                             const TorcBonjourService &Service)
     {
@@ -597,6 +689,15 @@ class TorcBonjourPriv
         }
     }
 
+/*! \fn    TorcBonjourPriv::Resolve
+ *  \brief Handle name resolution respones from Bonjour
+ *
+ * This function is called from the Bonjour library when a discovered service is resolved
+ * to a target and port. Before we can use this target, we attempt to discover its IP addresses
+ * via QHostInfo.
+ *
+ * \sa HostLookup
+*/
     void Resolve(DNSServiceRef Reference, DNSServiceErrorType ErrorType, const char *Fullname,
                  const char *HostTarget, uint16_t Port, uint16_t TxtLen,
                  const unsigned char *TxtRecord)
@@ -630,6 +731,12 @@ class TorcBonjourPriv
         }
     }
 
+/*! \fn    TorcBonjourPriv::HostLookup
+ *  \brief Handle host lookup responses from Qt.
+ *
+ * If the service is fully resolved to one or more IP addresses, notify the new
+ * service with a Torc::ServiceDiscovered event.
+*/
     void HostLookup(const QHostInfo &HostInfo)
     {
         // search for the lookup id
@@ -678,7 +785,6 @@ class TorcBonjourPriv
                 }
             }
         }
-
     }
 
   private:
@@ -690,6 +796,9 @@ class TorcBonjourPriv
     QMap<quint32,TorcBonjourService> m_discoveredServices;
 };
 
+/*! \fn    BonjourRegisterCallback
+ *  \brief Handles registration callbacks from Bonjour
+*/
 void BonjourRegisterCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
                      DNSServiceErrorType Errorcode,
                      const char *Name,   const char *Type,
@@ -715,6 +824,9 @@ void BonjourRegisterCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
     }
 }
 
+/*! \fn    BonjourBrowseCallback
+ *  \brief Handles browse callbacks from Bonjour
+*/
 void BonjourBrowseCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
                            uint32_t InterfaceIndex,
                            DNSServiceErrorType ErrorCode, const char *Name,
@@ -745,6 +857,9 @@ void BonjourBrowseCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
     }
 }
 
+/*! \fn    BonjourResolveCallback
+ *  \brief Handles address resolution callbacks from Bonjour
+*/
 void BonjourResolveCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
                             uint32_t InterfaceIndex, DNSServiceErrorType ErrorCode,
                             const char *Fullname, const char *HostTarget,
@@ -761,6 +876,28 @@ void BonjourResolveCallback(DNSServiceRef Ref, DNSServiceFlags Flags,
     bonjour->Resolve(Ref, ErrorCode, Fullname, HostTarget, Port, TxtLen, TxtRecord);
 }
 
+/*! \class TorcBonjour
+ *  \brief Advertises and searches for services via Bonjour (aka Zero Configuration Networking/Avahi)
+ *
+ * TorcBonjour returns opaque references for use by client objects. These are guaranteed to remain
+ * unchanged across network outages and other suspension events. Hence clients do not need to explicitly
+ * monitor network status and cancel advertisements and/or browse requests.
+ *
+ * \sa gBonjour
+ * \sa gBonjourLock
+ * \sa TorcBonjourPriv
+ * \sa TorcBonjourService
+ *
+ * \todo Send events to clients only (as for TorcSSDP)
+ * \todo Guard against or properly handle duplicate requests (registration or browse)
+ * \todo Make non-static public methods thread safe (Register, Browse, Deregister)
+*/
+
+/*! \fn    TorcBonjour::Instance
+ *  \brief Returns the global TorcBonjour singleton.
+ *
+ * If it does not exist, it will be created.
+*/
 TorcBonjour* TorcBonjour::Instance(void)
 {
     QMutexLocker locker(gBonjourLock);
@@ -771,13 +908,19 @@ TorcBonjour* TorcBonjour::Instance(void)
     return gBonjour;
 }
 
+/*! \fn    TorcBonjour::Suspend
+ *  \brief Suspends all Bonjour activities.
+*/
 void TorcBonjour::Suspend(bool Suspend)
 {
     QMutexLocker locker(gBonjourLock);
     if (gBonjour)
-        gBonjour->SuspendPriv(Suspend);
+        QMetaObject::invokeMethod(gBonjour, "SuspendPriv", Qt::AutoConnection, Q_ARG(bool, Suspend));
 }
 
+/*! \fn    TorcBonjour::TearDown
+ *  \brief Destroys the global TorcBonjour singleton.
+*/
 void TorcBonjour::TearDown(void)
 {
     QMutexLocker locker(gBonjourLock);
@@ -785,6 +928,9 @@ void TorcBonjour::TearDown(void)
     gBonjour = NULL;
 }
 
+/*! \fn    TorcBonjour::MapToTxtRecord
+ *  \brief Serialises a QMap into a Bonjour Txt record format.
+*/
 QByteArray TorcBonjour::MapToTxtRecord(const QMap<QByteArray, QByteArray> &Map)
 {
     QByteArray result;
@@ -800,6 +946,9 @@ QByteArray TorcBonjour::MapToTxtRecord(const QMap<QByteArray, QByteArray> &Map)
     return result;
 }
 
+/*! \fn    TorcBonjour::TxtRecordToMap
+ *  \brief Extracts a QMap from a properly formatted Bonjour Txt record
+*/
 QMap<QByteArray,QByteArray> TorcBonjour::TxtRecordToMap(const QByteArray &TxtRecord)
 {
     QMap<QByteArray,QByteArray> result;
@@ -827,14 +976,24 @@ TorcBonjour::TorcBonjour()
 
 TorcBonjour::~TorcBonjour()
 {
-    // stop listening for power events
+    // stop listening for network events
     gLocalContext->RemoveObserver(this);
 
-    // delete implementation
+    // delete private implementation
     delete m_priv;
     m_priv = NULL;
 }
 
+/*! \fn    TorcBonjour::Register
+ *  \brief Register a service for advertisement via Bonjour.
+ *
+ * Registers the service described by Name, Type and TxtRecords. The service will be advertised
+ * on all known system interfaces as available on port Port.
+ *
+ * \returns An opaque reference to be used via Deregister on success, 0 on failure.
+ *
+ * \sa Deregister
+*/
 quint32 TorcBonjour::Register(quint16 Port, const QByteArray &Type, const QByteArray &Name,
                               const QMap<QByteArray, QByteArray> &TxtRecords)
 {
@@ -847,6 +1006,16 @@ quint32 TorcBonjour::Register(quint16 Port, const QByteArray &Type, const QByteA
     return 0;
 }
 
+/*! \fn TorcBonjour::Browse
+ *  \brief Search for a service advertised via Bonjour
+ *
+ * Initiates a browse request for the service named by Type. The requestor must listen
+ * for the Torc::ServiceDiscovered and Torc::ServiceWentAway events.
+ *
+ * \returns An opaque reference to be used via Deregister on success, 0 on failure.
+ *
+ * \sa Deregister
+*/
 quint32 TorcBonjour::Browse(const QByteArray &Type)
 {
     if (m_priv)
@@ -854,24 +1023,50 @@ quint32 TorcBonjour::Browse(const QByteArray &Type)
     return 0;
 }
 
+/*! \fn    TorcBonjour::Deregister
+ *  \brief Cancel a Bonjour service registration or browse request.
+ *
+ * \param Reference A TorcBonjour reference previously obtained via a call to Register
+ * \sa Register
+ * \sa Browse
+*/
 void TorcBonjour::Deregister(quint32 Reference)
 {
     if (m_priv && Reference)
         m_priv->Deregister(Reference);
 }
 
+/*! \fn    TorcBonjour::socketReadyRead
+ *  \brief Read from a socket
+ *
+ * TorcBonjourPriv does not inherit QObject hence needs TorcBonjour to handle
+ * slots and events.
+*/
 void TorcBonjour::socketReadyRead(int Socket)
 {
     if (m_priv)
         m_priv->SocketReadyRead(Socket);
 }
 
+/*! \fn    TorcBonjour::HostLookup
+ *  \brief Handle a host lookup signal.
+ *
+ * TorcBonjourPriv does not inherit QObject hence needs TorcBonjour to handle
+ * slots and events.
+*/
 void TorcBonjour::HostLookup(const QHostInfo &HostInfo)
 {
     if (m_priv)
         m_priv->HostLookup(HostInfo);
 }
 
+/*! \fn TorcBonjour::event
+ *  \brief Implements QObject::event
+ *
+ * Listens for TorcNetwork related events and suspends or resumes Bonjour activities
+ * appropriately (although strictly speaking, this is probably not necessary as this
+ * will be handled internally by Bonjour).
+*/
 bool TorcBonjour::event(QEvent *Event)
 {
     if (Event->type() == TorcEvent::TorcEventType && m_priv)
