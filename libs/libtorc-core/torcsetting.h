8a4959592107ee2d7df147ae7d50e2c0843d2691
Mark Kendall <mark.kendall@gmail.com>
2013-07-03 13:16:52 +0100

    libtorc-core: Better Q_PROPERTY handling in TorcSetting

--- libs/libtorc-core/torcsetting.h
+++ libs/libtorc-core/torcsetting.h
@@ -30,13 +30,13 @@ class TORC_CORE_PUBLIC TorcSetting : public QAbstractListModel, public TorcRefer
 
   public:
     Q_PROPERTY (QVariant m_value       READ GetValue()       NOTIFY ValueChanged()       )
-    Q_PROPERTY (QString  m_uiName      READ GetName()        NOTIFY NameChanged()        )
-    Q_PROPERTY (QString  m_description READ GetDescription() NOTIFY DescriptionChanged() )
-    Q_PROPERTY (QString  m_helpText    READ GetHelpText()    NOTIFY HelpTextChanged()    )
-    Q_PROPERTY (QVariant m_default     READ GetDefault()     NOTIFY DefaultChanged()     )
-    Q_PROPERTY (bool     m_persistent  READ GetPersistent()  NOTIFY PersistentChanged()  )
+    Q_PROPERTY (QString  m_uiName      READ GetName()        CONSTANT                    )
+    Q_PROPERTY (QString  m_description READ GetDescription() CONSTANT                    )
+    Q_PROPERTY (QString  m_helpText    READ GetHelpText()    CONSTANT                    )
+    Q_PROPERTY (QVariant m_default     READ GetDefault()     CONSTANT                    )
+    Q_PROPERTY (bool     m_persistent  READ GetPersistent()  CONSTANT                    )
     Q_PROPERTY (int      m_isActive    READ IsActive()       NOTIFY ActiveChanged()      )
-    Q_PROPERTY (Type     m_type        READ GetSettingType   NOTIFY TypeChanged()        )
+    Q_PROPERTY (Type     m_type        READ GetSettingType   CONSTANT                    )
 
     // QAbstractListModel
     QVariant               data                 (const QModelIndex &Index, int Role) const;
@@ -81,12 +81,6 @@ class TORC_CORE_PUBLIC TorcSetting : public QAbstractListModel, public TorcRefer
     int                    GetStep              (void);
 
   signals:
-    void                   NameChanged          (const QString  &Name);    // QML compatabiliy only
-    void                   DescriptionChanged   (const QString  &Name);    // QML compatabiliy only
-    void                   HelpTextChanged      (const QString  &Name);    // QML compatabiliy only
-    void                   DefaultChanged       (const QVariant &Default); // QML compatabiliy only
-    void                   PersistentChanged    (bool Persistent);         // QML compatabiliy only
-    void                   TypeChanged          (Type NewType);            // QML compatabiliy only
     void                   ValueChanged         (const QVariant &Value);
     void                   ValueChanged         (int  Value);
     void                   ValueChanged         (bool Value);
