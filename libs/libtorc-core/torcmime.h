7d67a07db17600e16c601722505d4fd00cf385b5
Mark Kendall <mark.kendall@gmail.com>
2013-07-23 19:57:11 +0300

    libtorc-core: Add missing export decl for TorcMime

--- libs/libtorc-core/torcmime.h
+++ libs/libtorc-core/torcmime.h
@@ -6,17 +6,20 @@
 #include <QString>
 #include <QUrl>
 
-class TorcMime
+// Torc
+#include "torccoreexport.h"
+
+class TORC_CORE_PUBLIC TorcMime
 {
   public:
-    static QString MimeTypeForData            (const QByteArray &Data);
-    static QString MimeTypeForData            (QIODevice *Device);
-    static QString MimeTypeForFileNameAndData (const QString &FileName, QIODevice *Device);
-    static QString MimeTypeForFileNameAndData (const QString &FileName, const QByteArray &Data);
-    static QString MimeTypeForName            (const QString &Name);
-    static QString MimeTypeForUrl             (const QUrl &Url);
-    static QStringList MimeTypeForFileName    (const QString &FileName);
-    static QStringList ExtensionsForType      (const QString &Type);
+    static QString      MimeTypeForData             (const QByteArray &Data);
+    static QString      MimeTypeForData             (QIODevice *Device);
+    static QString      MimeTypeForFileNameAndData  (const QString &FileName, QIODevice *Device);
+    static QString      MimeTypeForFileNameAndData  (const QString &FileName, const QByteArray &Data);
+    static QString      MimeTypeForName             (const QString &Name);
+    static QString      MimeTypeForUrl              (const QUrl &Url);
+    static QStringList  MimeTypeForFileName         (const QString &FileName);
+    static QStringList  ExtensionsForType           (const QString &Type);
 };
 
 #endif // TORCMIME_H
