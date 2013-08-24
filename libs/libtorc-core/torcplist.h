a93ec98f8f52cb113d2391e14fc7d04a032b9d34
Mark Kendall <mark.kendall@gmail.com>
2013-08-24 23:04:59 +0100

    libtorc-core: TorcPList whitespace.

--- libs/libtorc-core/torcplist.h
+++ libs/libtorc-core/torcplist.h
@@ -11,30 +11,30 @@
 class TORC_CORE_PUBLIC TorcPList
 {
   public:
-    TorcPList       (const QByteArray &data);
+    TorcPList       (const QByteArray Data);
 
-    QVariant        GetValue(const QString &Key);
-    QString         ToString(void);
-    bool            ToXML(QIODevice *Device);
+    QVariant        GetValue           (const QString &Key);
+    QString         ToString           (void);
+    bool            ToXML              (QIODevice *Device);
 
   private:
-    void            ParseBinaryPList  (const QByteArray &Data);
-    QVariant        ParseBinaryNode   (quint64 Num);
-    QVariantMap     ParseBinaryDict   (quint8 *Data);
-    QList<QVariant> ParseBinaryArray  (quint8 *Data);
-    QVariant        ParseBinaryString (quint8 *Data);
-    QVariant        ParseBinaryReal   (quint8 *Data);
-    QVariant        ParseBinaryDate   (quint8 *Data);
-    QVariant        ParseBinaryData   (quint8 *Data);
-    QVariant        ParseBinaryUnicode(quint8 *Data);
-    QVariant        ParseBinaryUInt   (quint8 **Data);
-    quint64         GetBinaryCount    (quint8 **Data);
-    quint64         GetBinaryUInt     (quint8 *Data, quint64 Size);
-    quint8*         GetBinaryObject   (quint64 Num);
+    void            ParseBinaryPList   (const QByteArray &Data);
+    QVariant        ParseBinaryNode    (quint64 Num);
+    QVariantMap     ParseBinaryDict    (quint8 *Data);
+    QList<QVariant> ParseBinaryArray   (quint8 *Data);
+    QVariant        ParseBinaryString  (quint8 *Data);
+    QVariant        ParseBinaryReal    (quint8 *Data);
+    QVariant        ParseBinaryDate    (quint8 *Data);
+    QVariant        ParseBinaryData    (quint8 *Data);
+    QVariant        ParseBinaryUnicode (quint8 *Data);
+    QVariant        ParseBinaryUInt    (quint8 **Data);
+    quint64         GetBinaryCount     (quint8 **Data);
+    quint64         GetBinaryUInt      (quint8 *Data, quint64 Size);
+    quint8*         GetBinaryObject    (quint64 Num);
 
-    bool            ToXML             (const QVariant &Data, QXmlStreamWriter &XML);
-    void            DictToXML         (const QVariant &Data, QXmlStreamWriter &XML);
-    void            ArrayToXML        (const QVariant &Data, QXmlStreamWriter &XML);
+    bool            ToXML              (const QVariant &Data, QXmlStreamWriter &XML);
+    void            DictToXML          (const QVariant &Data, QXmlStreamWriter &XML);
+    void            ArrayToXML         (const QVariant &Data, QXmlStreamWriter &XML);
 
   private:
     QVariant        m_result;
