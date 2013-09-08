a6e7428ebcbce849c37a28de402c40bc2346e6a0
Mark Kendall <mark.kendall@gmail.com>
2013-09-08 09:15:26 +0100

    docs: Add mainpage.cpp
    - which I thought I'd added weeks ago:)

---
+++ docs/mainpage.cpp
@@ -0,0 +1,37 @@
+/*! \mainpage Torc
+ *
+ * \section intro Introduction
+ * Torc is an open source, cross-platform media centre.
+ *
+ * \section programs Programs
+ * There are currently 3 programs:
+ *   - torc-server
+ *     - A headless media server and player.
+ *
+ *   - torc-desktop
+ *     - The Torc desktop application with a 'traditional' mouse/keyboard driven interface.
+ *
+ *   - torc-tv
+ *     - Designed for display on a 'ten foot interface' and optimised for remote control,
+ *       torc-tv is analagous to a set top box.
+ *
+ *   - torc-mobile (tbc)
+ *
+ *   - torc-utils
+ *     - A simple command line application to probe and play media files.
+ *
+ * \section torclibraries Internal libraries
+ * The majority of the code is contained within a group of shared libraries:
+ * \subsection libtorc-core libtorc-core
+ *   - libtorc-audio
+ *   - libtorc-video
+ *   - libtorc-baseui
+ *   - libtorc-tenfootui
+ *   - libtorc-videoui
+ *   - libtorc-media
+ *   - libtorc-av
+ * ![](../images/library-inheritance.svg) "Torc library inheritance"
+ *
+ * \section extlibraries External libraries
+ *
+ */
