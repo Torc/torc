e7f842c4dc927a6398e70d6a1fe6794625fa37c2
Mark Kendall <mark.kendall@gmail.com>
2013-11-07 08:28:15 +0000

    libtorc-audio: Missing license and (basic) docs for TorcCDBuffer

--- libs/libtorc-audio/cdaudio/torccdbuffer.cpp
+++ libs/libtorc-audio/cdaudio/torccdbuffer.cpp
@@ -1,7 +1,36 @@
+/* Class TorcCDBuffer
+*
+* This file is part of the Torc project.
+*
+* Copyright (C) Mark Kendall 2012-13
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+* USA.
+*/
+
 // Torc
 #include "torclogging.h"
 #include "torccdbuffer.h"
 
+/*! \class TorcCDBuffer
+ *  \brief A simple TorcBuffer wrapper that creates the correct libcdio based input context for audio CD playback.
+ *
+ * \todo Replace the FFmpeg implementation with an internal version that handles all metadata lookups etc.
+ *
+ * \sa TorcBuffer
+*/
 TorcCDBuffer::TorcCDBuffer(const QString &URI, int *Abort)
   : TorcBuffer(URI, Abort),
     m_input(NULL)
@@ -37,7 +66,7 @@ bool TorcCDBuffer::Open(void)
     if (!m_input)
         LOG(VB_GENERAL, LOG_ERR, "libcdio demuxer not found");
 
-    return m_input;
+    return m_input != NULL;
 }
 
 void TorcCDBuffer::Close(void)
@@ -97,6 +126,9 @@ int TorcCDBuffer::BestBufferSize(void)
     return 0;
 }
 
+/*! \class TorcCDBufferFactory
+ *  \brief A static class to create an audio CD input buffer.
+*/
 static class TorcCDBufferFactory : public TorcBufferFactory
 {
     void Score(const QString &URI, const QUrl &URL, int &Score, const bool &Media)
