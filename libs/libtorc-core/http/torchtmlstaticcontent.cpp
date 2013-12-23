571d44bc8f67a118c9f99b898a3728749b6523b3
Mark Kendall <mark.kendall@gmail.com>
2013-12-23 16:31:36 +0000

    libtorc-core: Add torcconfiguration.js generation.
    - this is a simple javascript object that should encapsulate Torc
    constants, variables and translated strings and is available to the rest
    of the javascript environment via 'torc'.

--- libs/libtorc-core/http/torchtmlstaticcontent.cpp
+++ libs/libtorc-core/http/torchtmlstaticcontent.cpp
@@ -26,9 +26,11 @@
 // Torc
 #include "torclogging.h"
 #include "torcdirectories.h"
+#include "torcnetwork.h"
 #include "torchttpserver.h"
 #include "torchttprequest.h"
 #include "torchttpservice.h"
+#include "torchttpconnection.h"
 #include "torchtmlstaticcontent.h"
 
 /*! \class TorcHTMLStaticContent
@@ -41,7 +43,7 @@ TorcHTMLStaticContent::TorcHTMLStaticContent()
     m_recursive = true;
 }
 
-void TorcHTMLStaticContent::ProcessHTTPRequest(TorcHTTPRequest *Request, TorcHTTPConnection*)
+void TorcHTMLStaticContent::ProcessHTTPRequest(TorcHTTPRequest *Request, TorcHTTPConnection* Connection)
 {
     if (!Request)
         return;
@@ -55,11 +57,23 @@ void TorcHTMLStaticContent::ProcessHTTPRequest(TorcHTTPRequest *Request, TorcHTT
         return;
     }
 
-    // get the required path
+    // get the requested file subpath
+    QString subpath = Request->GetUrl();
+
+    // request for Torc configuration. This is used to add some appropriate Javascript globals
+    // and translated strings
+
+    if (subpath == STATIC_DIRECTORY + "js/torcconfiguration.js")
+    {
+        GetJavascriptConfiguration(Request, Connection);
+        return;
+    }
+
+    // get the local path to static content
     QString path = GetTorcShareDir();
     if (path.endsWith("/"))
         path.chop(1);
-    path += Request->GetUrl();
+    path += subpath;
 
     // file
     QFile *file = new QFile(path);
@@ -95,4 +109,74 @@ void TorcHTMLStaticContent::ProcessHTTPRequest(TorcHTTPRequest *Request, TorcHTT
 
     Request->SetResponseType(HTTPResponseNone);
     delete file;
+}
+
+/*! \brief Construct a Javascript object that encapsulates Torc variables, enumerations and translated strings.
+ *
+ * The contents of this object are available as 'torcconfiguration.js'.
+ *
+ * \todo Translations are static and will need to be regenerated if the language is changed.
+*/
+void TorcHTMLStaticContent::GetJavascriptConfiguration(TorcHTTPRequest *Request, TorcHTTPConnection *Connection)
+{
+    if (!Request)
+        return;
+
+    // populate the list of static constants and translations
+    static QMap<QString,QString> statics;
+    static bool initialised = false;
+
+    if (!initialised)
+    {
+        initialised = true;
+
+        statics.insert("SocketNotConnected", QObject::tr("Not connected"));
+        statics.insert("SocketConnecting", QObject::tr("Connecting"));
+        statics.insert("SocketConnected", QObject::tr("Connected"));
+    }
+
+    // generate this instance's authority (host + port)
+    QString authority = (Connection && Connection->GetSocket()) ? TorcNetwork::IPAddressToLiteral(Connection->GetSocket()->localAddress()) : "unknown";
+    authority += ":" + QString::number(TorcHTTPServer::GetPort());
+
+    // generate dynamic variables
+    QMap<QString,QString> dynamics;
+    dynamics.insert("ServerAuthority", authority);
+    dynamics.insert("ServicesPath", SERVICES_DIRECTORY);
+
+    // and generate javascript
+    QByteArray *result = new QByteArray();
+    QTextStream stream(result);
+
+    stream << QString("var torc = {\r\n");
+    bool first = true;
+    QMap<QString,QString>::iterator it = statics.begin();
+    for ( ; it != statics.end(); ++it)
+    {
+        if (!first)
+            stream << ",\r\n";
+        stream << "  ";
+        first = false;
+        stream << it.key() << ": '" << it.value() << "'";
+    }
+
+    it = dynamics.begin();
+    for ( ; it != dynamics.end(); ++it)
+    {
+        if (!first)
+            stream << ",\r\n";
+        stream << "  ";
+        first = false;
+        stream << it.key() << ": '" << it.value() << "'";
+    }
+
+    if (!first)
+        stream << "\r\n";
+    stream << QString("};\r\n\r\n");
+    stream << QString("if (Object.freeze) { Object.freeze(torc); }\r\n");
+
+    stream.flush();
+    Request->SetStatus(HTTP_OK);
+    Request->SetResponseType(HTTPResponseJSONJavascript);
+    Request->SetResponseContent(result);
 }
--- libs/libtorc-core/http/torchtmlstaticcontent.h
+++ libs/libtorc-core/http/torchtmlstaticcontent.h
@@ -9,7 +9,10 @@ class TorcHTMLStaticContent : public TorcHTTPHandler
   public:
     TorcHTMLStaticContent();
 
-    void ProcessHTTPRequest (TorcHTTPRequest *Request, TorcHTTPConnection*);
+    void ProcessHTTPRequest          (TorcHTTPRequest *Request, TorcHTTPConnection* Connection);
+
+  protected:
+    void GetJavascriptConfiguration  (TorcHTTPRequest *Request, TorcHTTPConnection* Connection);
 };
 
 #endif // TORCHTMLSTATICCONTENT_H
