38659c812ba648dc0a0fecd07ad3fb5185738ae3
Mark Kendall <mark.kendall@gmail.com>
2014-01-31 16:30:53 +0000

    html: Replace glyphicons with fontawesome.
    - adds consistency with the qml interface.
    - fontawesome has more icons and more options

--- html/js/torc.js
+++ html/js/torc.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
             class: 'dropdown-toggle',
             href:  '#'})
             .attr('data-toggle', 'dropdown')
-                  .append($('<span/>', { class: 'glyphicon ' + toggleClass})))
+                  .append($('<i/>', { class: 'fa ' + toggleClass + ' fa-lg'})))
           .append($('<ul/>', {
             class: 'dropdown-menu ' + listClass,
             role: 'menu'
@@ -53,7 +53,7 @@ $(document).ready(function() {
                 value.forEach( function (element, index) {
                     addDropdownMenuItem('torc-peer-menu', 'torc-peer torc-peer' + index, 'http://' + element.address + ':' + element.port + '/html/index.html', '');
                     qsTranslate('TorcNetworkedContext', 'Connect to %1', '', 0,
-                                function(result) { $(".torc-peer" + index + " a").html('<span class=\'glyphicon glyphicon-share\'>&nbsp</span>' + result.replace("%1", element.name)); });
+                                function(result) { $(".torc-peer" + index + " a").html('<i class=\'fa fa-external-link-square\'>&nbsp</i>' + result.replace("%1", element.name)); });
                 });
             } else {
                 addDropdownMenuItem('torc-peer-menu', 'torc-peer-status', '#', torc.NoPeers);
@@ -64,7 +64,7 @@ $(document).ready(function() {
     function peerSubscriptionChanged(version, methods, properties) {
         if (version !== undefined && typeof properties === 'object' && properties.hasOwnProperty('peers') &&
             properties.peers.hasOwnProperty('value') && $.isArray(properties.peers.value)) {
-            addNavbarDropdown('torc-peer-dropdown', 'glyphicon-link', 'torc-peer-menu');
+            addNavbarDropdown('torc-peer-dropdown', 'fa-link', 'torc-peer-menu');
             peerListChanged('peers', properties.peers.value);
             return;
         }
@@ -92,31 +92,31 @@ $(document).ready(function() {
             if (value === undefined) {
                 translatedName = '';
             } else if (value === torc.ACPower) {
-                translatedName = '<span class=\'glyphicon glyphicon-flash\'>&nbsp</span>' + torc.ACPowerTr;
+                translatedName = '<i class=\'fa fa-bolt\'>&nbsp</i>' + torc.ACPowerTr;
             } else if (value === torc.UnknownPower) {
                 translatedName = torc.UnknownPowerTr;
             } else {
-                translatedName = '<span class=\'glyphicon glyphicon-dashboard\'>&nbsp</span>' + value + '%';
+                translatedName = '<i class=\'fa fa-tachometer\'>&nbsp</i>' + value + '%';
                 qsTranslate('TorcPower', 'Battery %n%', '', value,
-                            function (result) { $('.torc-power-status a').html('<span class=\'glyphicon glyphicon-dashboard\'>&nbsp</span>' + result); });
+                            function (result) { $('.torc-power-status a').html('<i class=\'fa fa-tachometer\'>&nbsp</i>' + result); });
             }
 
             $('.torc-power-status a').html(translatedName);
             return;
         } else if (name === 'canSuspend') {
-            translatedName = '<span class=\'glyphicon glyphicon-remove\'>&nbsp</span>' + torc.Suspend;
+            translatedName = '<i class=\'fa fa-times\'>&nbsp</i>' + torc.Suspend;
             translatedConfirmation = torc.ConfirmSuspend;
             method = 'Suspend';
         } else if (name === 'canShutdown') {
-            translatedName = '<span class=\'glyphicon glyphicon-remove-circle\'>&nbsp</span>' + torc.Shutdown;
+            translatedName = '<i class=\'fa fa-times-circle\'>&nbsp</i>' + torc.Shutdown;
             translatedConfirmation = torc.ConfirmShutdown;
             method = 'Shutdown';
         } else if (name === 'canHibernate') {
-            translatedName = '<span class=\'glyphicon glyphicon-remove\'>&nbsp</span>' + torc.Hibernate;
+            translatedName = '<i class=\'fa fa-moon-o\'>&nbsp</i>' + torc.Hibernate;
             translatedConfirmation = torc.ConfirmHibernate;
             method = 'Hibernate';
         } else if (name === 'canRestart') {
-            translatedName = '<span class=\'glyphicon glyphicon-refresh\'>&nbsp</span>' + torc.Restart;
+            translatedName = '<i class=\'fa fa-refresh\'>&nbsp</i>' + torc.Restart;
             translatedConfirmation = torc.ConfirmRestart;
             method = 'Restart';
         } else { return; }
@@ -134,7 +134,7 @@ $(document).ready(function() {
 
     function powerSubscriptionChanged(version, methods, properties) {
         if (version !== undefined && typeof properties === 'object') {
-            addNavbarDropdown('torc-power-dropdown', 'glyphicon-off', 'torc-power-menu');
+            addNavbarDropdown('torc-power-dropdown', 'fa-power-off', 'torc-power-menu');
             addDropdownMenuItem('torc-power-menu', 'torc-power-status', '#', '');
             addDropdownMenuDivider('torc-power-menu', '');
             powerChanged('canSuspend', properties.canSuspend.value);
@@ -150,15 +150,15 @@ $(document).ready(function() {
 
     function statusChanged (status) {
         if (status === torc.SocketNotConnected) {
-            $(".torc-socket-status-glyph").removeClass("glyphicon-ok glyphicon-ok-circle glyphicon-question-sign").addClass("glyphicon-exclamation-sign");
+            $(".torc-socket-status-icon").removeClass("fa-check fa-check-circle-o-circle-o fa-question-circle").addClass("fa-exclamation-circle");
             $(".torc-socket-status-text a").html(torc.SocketNotConnected);
         } else if (status === torc.SocketConnecting) {
-            $(".torc-socket-status-glyph").removeClass("glyphicon-ok glyphicon-ok-circle glyphicon-exclamation-sign").addClass("glyphicon-question-sign");
+            $(".torc-socket-status-icon").removeClass("fa-check fa-check-circle-o fa-exclamation-circle").addClass("fa-question-circle");
             $(".torc-socket-status-text a").html(torc.SocketConnecting);
         } else if (status === torc.SocketConnected) {
-            $(".torc-socket-status-glyph").removeClass("glyphicon-exclamation-sign glyphicon-ok-circle glyphicon-question-sign").addClass("glyphicon-ok");
+            $(".torc-socket-status-icon").removeClass("fa-exclamation-circle fa-check-circle-o fa-question-circle").addClass("fa-check");
         } else if (status === torc.SocketReady) {
-            $(".torc-socket-status-glyph").removeClass("glyphicon-ok glyphicon-exclamation-sign glyphicon-question-sign").addClass("glyphicon-ok-circle");
+            $(".torc-socket-status-icon").removeClass("fa-check fa-exclamation-circle fa-question-circle").addClass("fa-check-circle-o");
             torcconnection.subscribe('languages', ['languageString', 'languages'], languageChanged, languageSubscriptionChanged);
             torcconnection.subscribe('peers', ['peers'], peerListChanged, peerSubscriptionChanged);
             torcconnection.subscribe('power', ['canShutdown', 'canSuspend', 'canRestart', 'canHibernate', 'batteryLevel'], powerChanged, powerSubscriptionChanged);
@@ -169,7 +169,7 @@ $(document).ready(function() {
     $(".navbar-brand").html(torc.ServerApplication);
 
     // add a socket status/connection dropdown with icon
-    addNavbarDropdown('', 'torc-socket-status-glyph', 'torc-socket-menu');
+    addNavbarDropdown('', 'torc-socket-status-icon', 'torc-socket-menu');
 
     // add the connection text item
     addDropdownMenuItem('torc-socket-menu', 'torc-socket-status-text', '#', '');
